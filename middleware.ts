80a916d17be729f8d20dc68ca44a022dd0b0998d
Shivam <shekharshivam51@gmail.com>
2024-05-18 11:59:03 +0530

    middleware updated

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs/server";
 export default authMiddleware({
   publicRoutes: ['/', '/api/webhooks/clerk']
 });
-
+ 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
